df8dc6c844974a627b73384207ea479028805fea
Krzysztof Pluciński <krzysztof.plucinski95@gmail.com>
2018-03-25 19:46:58 +0200

    Created simple gameloop

--- MasterMind.ino
+++ MasterMind.ino
@@ -1,12 +1,46 @@
+#include <SPI.h>
+#include <Gamebuino.h>
 
+Gamebuino gb;
+
+
+void setGameTitle()
+{
+  gb.titleScreen(F("Mastermind"));
+}
 
 void setup()
+{
+  gb.begin();
+  setGameTitle();
+}
+
+void input()
+{
+  if(gb.buttons.pressed(BTN_C))
+  {
+    setGameTitle();
+  }
+}
+
+void update()
 {
   
 }
 
-void loop()
+void draw()
 {
   
+}
+
+
+void loop()
+{
+  if(gb.update())
+  {
+    input();
+    update();
+    draw();
+  }
 }
 
