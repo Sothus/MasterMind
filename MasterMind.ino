46a8debe8ac4e06220e45c900c4447ee4fbd7726
Sothus <krzysztof.plucinski.95@gmail.com>
2018-03-29 22:00:13 +0200

    Layout tests

--- MasterMind.ino
+++ MasterMind.ino
@@ -67,18 +67,22 @@ class CircleChar: public Circle
 void CircleChar::draw()
 {
   Circle::draw();
-  gb.display.drawChar(this->x_ - 1, this->y_ - 2, this->char_,1);
+  gb.display.drawChar(this->x_ - 1, this->y_ - 2, this->char_, 1);
   
 }
 
 
 /**************GAME LOGIC********************/
-CircleChar crArray[] = {CircleChar(8, 40, 5, '2'),
-                        CircleChar(20, 40, 5, '2'),
-                        CircleChar(32, 40, 5, '2'),
-                        CircleChar(44, 40, 5, '2'),
+CircleChar crArray[] = {CircleChar(6 , 41, 4, '2'),
+                        CircleChar(18, 41, 4, '2'),
+                        CircleChar(30, 41, 4, '2'),
+                        CircleChar(42, 41, 4, '2'),
+                        CircleChar(54, 41, 4, '2'),
+                        CircleChar(6 , 29, 4, '2'),
+                        CircleChar(6 , 17, 4, '2'),
+                        CircleChar(6 , 5, 4, '2'),
                       };
-int crArrayCount = 4;
+int crArrayCount = 8;
 
 void setup()
 {
@@ -86,6 +90,7 @@ void setup()
   gb.begin();
   goToGameTitle();
 }
+
 
 void loop()
 {
@@ -109,21 +114,12 @@ void loop()
   }
 }
 
+
 void goToGameTitle()
 {
   gb.titleScreen(F("Mastermind"));
 }
 
-void draw()
-{
-  Serial.println(F("DRAWING"));
-  for(int i = 0; i < crArrayCount; i++)
-  {
-    
-    crArray[i].draw();
-  }
-  
-}
 
 void game()
 {
@@ -138,5 +134,39 @@ void game()
   }
 }
 
+//84x48 
+void draw()
+{
+  Serial.println(F("DRAWING"));
+  for(int i = 0; i < crArrayCount; i++)
+  {
+    
+    crArray[i].draw();
+  }
+  drawGameMesh();
+  drawScores();
+  
+}
+
+void drawGameMesh()
+{
+  Serial.println(F("DRAWING MESH"));
+  gb.display.setColor(GRAY);
+  gb.display.drawFastHLine(0, 35, 84);
+  gb.display.drawFastHLine(0, 23, 84);
+  gb.display.drawFastHLine(0, 11, 84);
+  gb.display.drawFastVLine(60, 0, 48);
+}
+
+void drawScores()
+{
+  gb.display.setColor(BLACK);
+  gb.display.drawChar(62, 39, '+', 1);
+  gb.display.drawChar(65, 39, '1', 1);
+  gb.display.drawChar(69, 39, '-', 1);
+  gb.display.drawChar(72, 39, '1', 1);
+  gb.display.drawChar(77, 39, '?', 1);
+  gb.display.drawChar(80, 39, '1', 1);
+}
 
 
